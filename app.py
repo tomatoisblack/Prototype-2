f8a42290cb4f191e29a5a83eeedbc87ddea0c1b2
tomatoisblack <162192892+tomatoisblack@users.noreply.github.com>
2025-10-03 19:09:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,6 @@ model = "gpt-4-1106-preview"  # "gpt-3.5-turbo-16k"
 filepath = "./cryptocurrency.pdf"
 file_object = client.files.create(file=open(filepath, "rb"), purpose="assistants")
 
-# Step 2 - Create an assistant
-# assistant = client.beta.assistants.create(
-#     name="Studdy Buddy",
-#     instructions="""You are a helpful study assistant who knows a lot about understanding research papers.
-#     Your role is to summarize papers, clarify terminology within context, and extract key figures and data.
-#     Cross-reference information for additional insights and answer related questions comprehensively.
-#     Analyze the papers, noting strengths and limitations.
-#     Respond to queries effectively, incorporating feedback to enhance your accuracy.
-#     Handle data securely and update your knowledge base with the latest research.
-#     Adhere to ethical standards, respect intellectual property, and provide users with guidance on any limitations.
-#     Maintain a feedback loop for continuous improvement and user support.
-#     Your ultimate goal is to facilitate a deeper understanding of complex scientific material, making it more accessible and comprehensible.""",
-#     tools=[{"type": "retrieval"}],
-#     model=model,
-#     file_ids=[file_object.id],
-# )
-
 # === Get the Assis ID ===
 # assis_id = assistant.id
 # print(assis_id)
